bcdb6ffdc2c1be941ed4ea7a7ecdda7d2565e9f3
GarlGaru <g@g>
2025-03-10 21:57:14 +0900

    다크모드 svg로 변경

--- darkMode.js
+++ darkMode.js
@@ -1,23 +1,25 @@
 
 const darkToggle = document.getElementById('darkModeToggle');
 
+const moonIconPath = 'svgs/moon.svg';
+const sunIconPath = 'svgs/sun.svg';
 
 darkToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark-mode');
     document.querySelectorAll('.highlight-teal_background').forEach(highlight => {
-        highlight.classList.toggle('dark');
+        highlight.classList.toggle('dark-mode');
     });
     document.querySelectorAll('.highlight-yellow_background').forEach(highlight => {
-        highlight.classList.toggle('dark');
+        highlight.classList.toggle('dark-mode');
     });
     document.querySelectorAll('hr').forEach(highlight => {
-        highlight.classList.toggle('dark');
+        highlight.classList.toggle('dark-mode');
     });
 
-    if(document.body.classList.contains('dark')) {
-        darkToggle.textContent = '☀️';
+    if(document.body.classList.contains('dark-mode')) {
+        darkToggle.src = sunIconPath;
     } else {
-        darkToggle.textContent = '🌙';
+        darkToggle.src = moonIconPath;
     }
 });
 
